f95edeb9a5153978220340cb6b53b2e26474a0db
littlestone3 <littlestone3@yahoo.com>
2022-01-25 02:33:59 -0500

    add sprocs

---
+++ RestBook/StoredProcedures/GetSortedBooksAuthorTitleSproc.sql
@@ -0,0 +1,16 @@
+﻿USE [RestBook]
+GO
+/****** Object:  StoredProcedure [dbo].[GetSortedBooksAuthorTitleSproc]    Script Date: 1/25/2022 2:32:39 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+ALTER PROCEDURE [dbo].[GetSortedBooksAuthorTitleSproc]
+as 
+begin
+	select * 
+	from dbo.Books
+	order by AuthorLastName, AuthorFirstName, Title
+
+end
---
+++ RestBook/StoredProcedures/GetSortedBooksPublisherAuthorTitleSproc.sql
@@ -0,0 +1,16 @@
+﻿USE [RestBook]
+GO
+/****** Object:  StoredProcedure [dbo].[GetSortedBooksPublisherAuthorTitleSproc]    Script Date: 1/25/2022 2:33:11 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+ALTER PROCEDURE [dbo].[GetSortedBooksPublisherAuthorTitleSproc]
+as 
+begin
+	select * 
+	from dbo.Books
+	order by Publisher, AuthorLastName, AuthorFirstName, Title
+
+end
